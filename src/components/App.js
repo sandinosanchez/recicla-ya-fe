eed454a6439288913a162b7a47059e297f3df619
sandinosanchez <sandino320@gmail.com>
2019-11-01 12:33:10 -0300

    avances en el feed, no esta terminado

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="" component={HomePage} />
+        <Route exact path="login/" component={LoginPage} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import CreatePost from '../components/CreatePost';
+import PostView from '../components/PostView';
+import '../css/post.css';
 
 export default class HomePage extends React.Component{
 
@@ -13,7 +16,14 @@ export default class HomePage extends React.Component{
 	render(){
 		return(
 			<div>
-				<h1> LA PAGINA PERRO </h1>
+			    <div className="container gedf-wrapper">
+			        <div className="row">
+			            <div className="col-md-8 gedf-main">
+			            	<CreatePost />
+			                <PostView />
+			        	</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
